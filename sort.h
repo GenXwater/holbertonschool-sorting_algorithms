f73e995ddcfd7badf7ef793b0229c54206b232fe
GenXwater <louis.genty@outlook.com>
2024-08-09 15:59:09 +0200

    added a functions for quick_sort

--- sort.h
+++ sort.h
@@ -25,6 +25,9 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int arr[], int low, int high, size_t size);
+int partition(int arr[], int low, int high);
+void swap(int* a, int* b);
 
 #endif
